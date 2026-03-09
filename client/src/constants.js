4488799d070a72023d306892baf51b6aa0bac58c
justin <jzou0417a@gmail.com>
2026-03-09 17:14:48 -0400

    Add colors and utility data to constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,14 +2,17 @@ export const UI = {
     TOO_SMALL: 1024,
 };
 
-export const UTILITY_OPTS = [
-    {id: "flashbang", label: "Flashbang"},
-    {id: "hegrenade", label: "HE"},
-    {id: "smokegrenade", label: "Smoke"},
-    {id: "molotov", label: "Molotov"},
-    {id: "incendiary", label: "Incendiary"},
-    {id: "decoy", label: "Decoy"},
-];
+
+export const UTILITY_DATA = {
+    flashbang: {id: "flashbang", label: "Flashbang"},
+    hegrenade: {id: "hegrenade", label: "HE"},
+    smokegrenade: {id: "smokegrenade", label: "Smoke"},
+    molotov: {id: "molotov", label: "Molotov"},
+    incendiary: {id: "incendiary", label: "Incendiary"},
+    decoy: {id: "decoy", label: "Decoy"},
+};
+
+export const UTILITY_OPTS = Object.values(UTILITY_DATA);
 
 export const TEAM_OPTS = [
     {id: "ALL", label: "Both"},
@@ -19,5 +22,7 @@ export const TEAM_OPTS = [
 
 export const COLORS = {
     TEAM_ONE: "#ff4d4d",
-    TEAM_TWO: "#4d4dff"
+    TEAM_TWO: "#4d4dff",
+    POS_CONNOTATION: "#33ff00",
+    NEG_CONNOTATION: "#ff5151",
 }
