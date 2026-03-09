632aa16799fffc1f88c3b6b94d439d7e507c94f1
justin <jzou0417a@gmail.com>
2026-03-08 23:37:02 -0400

    Scaffold UtilityOverlay

---
+++ client/src/components/widgets/UtilityOverlay.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { gameToAppAxis } from '../../util/plot';
+import { ref } from 'vue';
+import { useMatchStore } from '../../stores/useMatchStore';
+import { storeToRefs } from 'pinia';
+
+const store = useMatchStore();
+const { filteredMatchNades } = storeToRefs(store);
+
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
