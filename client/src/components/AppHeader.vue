08f413e643105f2a278fa42add5830b29d3ee097
justin <jzou0417a@gmail.com>
2026-03-13 02:22:13 -0400

    Add favicon / change styling on header

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -3,7 +3,7 @@
 
 <template>
     <header>
-        <h1><img src="/flashbang-icon.png" alt="logo">NadeAnalyzer</h1>
+        <h1><img src="/favicon-raw.png" alt="logo">util-analyzer 2</h1>
         <div class="header__sub">
             <p><a href="https://steamcommunity.com/my/gcpd/730?tab=matchhistorypremier" target="_blank">Find your .dem files here.</a></p>
             <button id="upload-btn">Upload .dem file</button>
@@ -16,8 +16,6 @@ header {
   display: flex;
   justify-content: space-between;
   padding: 1rem;
-  background-color: #1a1a1a;
-  color: white; /* Inherited from body usually, but good to be explicit if isolated */
 }
 
 .header__sub {
