11ff593654508153328169ff5543ea0e1770e072
justin <jzou0417a@gmail.com>
2026-03-13 01:14:49 -0400

    Improve demo path resolution

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,11 +1,16 @@
 import {setGlobalOptions} from "firebase-functions/v2";
 import {onCall, HttpsError} from "firebase-functions/v2/https";
 import { extractMatchData} from "./parse-evts.js"
+import * as path from "path";
+import * as fs from "fs";
+
 setGlobalOptions({maxInstances: 10});
 
-const demoPath = "test.dem";
+const demoPath = path.resolve("test.dem");
 
 export const fetchMatchData = onCall({memory: "1GiB", timeoutSeconds: 300, cors: true},async (req) => {
+  if (!fs.existsSync(demoPath)) {
+    throw new HttpsError("not-found", `demo file not found @ ${demoPath}`);
+  }
   return extractMatchData(demoPath);
 });
-
